921c35da2348151bb1000131f1e17c5b03f2f25d
GiniMoses <106941124+GiniMoses@users.noreply.github.com>
2022-08-07 09:59:27 +0700

    pop

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,34 @@
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+import 'package:pop1122/about_screen.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    
-  }
+  State<HomeScreen> createState() => _HomeScreenState();
+}
 
-  
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("HomeScreen"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) {
+                  return AboutScreen();
+                },
+              ),
+            )
+          },
+          child: Text("Go to about screen"),
+        ),
+      ),
+    );
+  }
 }
